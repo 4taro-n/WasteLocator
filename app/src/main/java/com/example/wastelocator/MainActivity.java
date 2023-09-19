3d09d032e0092ec3a0b6735e791e6ef5f20a0013
Ryutaro Nakasone <88942733+4taro-n@users.noreply.github.com>
2023-09-20 04:09:31 +1000

    second-commit

---
+++ app/src/main/java/com/example/wastelocator/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.wastelocator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import com.example.wastelocator.Utils.SharedPrefManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView titleTextView;
+    private Button loginButton;
+    private Button signupButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+//        if (SharedPrefManager.isLoggedIn()) {
+//            Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+//            startActivity(intent);
+//        } else {
+        setContentView(R.layout.activity_main);
+        //Linking the views
+        setViewIds();
+
+        //Setting Click listeners
+        loginButton.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+        });
+
+        signupButton.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+        });
+//        }
+    }
+
+    private void setViewIds() {
+        titleTextView = findViewById(R.id.title);
+        loginButton = findViewById(R.id.btnLogin);
+        signupButton = findViewById(R.id.btnSignup);
+    }
+}
